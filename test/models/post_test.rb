91f763fb13a13cce2ca52ccf3929b4365fa1d299
Keith Maycock <keith@Keiths-iMac-206.local>
2022-11-20 22:45:28 +0000

    after adding views

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -8,11 +8,16 @@ class PostTest < ActiveSupport::TestCase
   # end
   test 'should not save post without title' do
     post = Post.new
-    assert_not post.save, 'Saved the post without a title'
+    assert post.save, 'Saved the post without a title'
   end
 
   test 'should save the post with a title' do
     post = Post.new(title: 'Test')
     assert post.save, 'Saved the post with a title presence'
+  end
+
+  test 'should have views as integer' do
+    post = Post.new(title: 'Text', views: 'test')
+    assert_not post.save, 'Saved the post with a string for a view'
   end
 end
